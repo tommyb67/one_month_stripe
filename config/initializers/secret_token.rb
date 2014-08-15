cb6cce69329b89c920d60ce4af420941e733353d
Tom Brennan <tommybrennan@gmail.com>
2014-08-15 11:25:20 -0400

    created the one month stripe rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneMonthStripe::Application.config.secret_key_base = '7c23b3c55f5097f0c871f3a9fbf623dc191caef93603668ece8b22fe0308fca0fae3e27c35b54b3557e287e2ffd2c8346afe279cc1725ff510215f1ccae5b26b'
