b3aceca47da4bc1ed0591d52371a20baf63b1a27
Tom Brennan <tommybrennan@gmail.com>
2014-09-02 22:33:58 -0400

    added to the seeds file I think this will be in the master branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+growth_hacking = Product.create(title: "Growth Hacking", subtitle: "Crash Course", author: "Mattan Griffel", price: "4.99", sku: "GROHACK1", description: %{where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business.})
